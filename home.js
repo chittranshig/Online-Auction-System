a79e5b1ba4f865b8255f62a7c72757c142e9907e
chittranshig <chittranshig@gmail.com>
2024-11-27 09:28:47 +0530

    This is online auction system

---
+++ home.js
@@ -0,0 +1,7 @@
+
+const openLoginSignupButton = document.getElementById('openLoginSignup');
+
+openLoginSignupButton.addEventListener('click', () => {
+  window.location.href = 'index.html';
+  
+});
